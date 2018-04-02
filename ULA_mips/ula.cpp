6a1be878c5011d969f86925da5acdcacb722e996
Thiago Araujo da Silva <thiagos.araujo09@gmail.com>
2018-04-02 15:14:10 -0300

    Adding add,sub and SLT modules

--- ULA_mips/ula.cpp
+++ ULA_mips/ula.cpp
@@ -2,6 +2,32 @@
 
 using namespace std;
 
+SC_MODULE(ADD){
+	sc_in< sc_int<SIZE> > A, B;
+	sc_out< sc_int<SIZE+1> > Z;
+	sc_out<bool> C_OUT;
+	SC_CTOR(ADD){
+		SC_METHOD(proc);
+		sensitive << A << B;
+	}
+	void proc(){
+		Z = A + B;
+		C_OUT = Z[32];
+	}
+};
+
+SC_MODULE(SUB){
+	sc_in< sc_int<SIZE> > A, B;
+	sc_out< sc_int<SIZE> > Z;
+	SC_CTOR(SUB){
+		SC_METHOD(proc);
+		sensitive << A << B;
+	}
+	void proc(){
+		Z = A - B;
+	}
+};
+
 SC_MODULE(AND){
 	sc_in< sc_int<SIZE> > A, B;
 	sc_out< sc_int<SIZE> > Z;
@@ -59,5 +85,22 @@ SC_MODULE(BYPASS){
 	}
 	void proc(){
 		Z = A;
+	}
+};
+
+SC_MODULE(SLT){
+	sc_in< sc_int<SIZE> > A;
+	sc_out< sc_int<SIZE> > Z;
+	SC_CTOR(SLT){
+		SC_METHOD(proc);
+		sensitive << A;
+	}
+	void proc(){
+		if(A[31] == '1'){
+			Z = {0};
+			Z[31] = 1;
+		}
+		else
+			Z = {0};
 	}
 };
