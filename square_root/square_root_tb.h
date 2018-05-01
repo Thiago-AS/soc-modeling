db478b03c5d142fade5be176a32748de0a35fd3e
Thiago Araujo <thiagos.araujo09@gmail.com>
2018-05-01 17:41:51 -0300

    creating testbench structure

---
+++ square_root/square_root_tb.h
@@ -0,0 +1,28 @@
+#ifndef SQUARE_ROOT_TB_H_
+#define SQUARE_ROOT_TB_H_
+
+#include "systemc.h"
+#include <iostream>
+
+SC_MODULE(square_root_tb){
+	sc_out<bool>rst;
+	sc_out<bool>start;
+	sc_out<int>num_in;
+
+	sc_in<int>num_out;
+	sc_in<bool>ready;
+	sc_in<bool>clk;
+
+	void source();
+	void sink();
+
+	SC_CTOR(square_root_tb){
+		SC_CTHREAD(source, clk.pos());
+		SC_CTHREAD(sink, clk.pos());
+	}
+
+};
+
+
+
+#endif
