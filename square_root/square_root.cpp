cc56b2a1f5b691433398f716d385defa7b623d11
Thiago Araujo <thiagos.araujo09@gmail.com>
2018-05-01 17:18:06 -0300

    Implementing thread functionality and sqrt algorithm

---
+++ square_root/square_root.cpp
@@ -0,0 +1,27 @@
+#include "square_root.h"
+
+void square_root::compute(){
+	//Reset section
+	ready.write(false);
+	num_out.write(0);
+
+	while(true){
+		if(start.read()){
+			int num_out_temp = sqrt(num_in.read());
+			num_out.write(num_out_temp);
+			ready.write(true);
+			wait();
+			ready.write(false);
+		}
+		wait();
+	}
+}
+
+
+int square_root::sqrt(unsigned num){
+	int r = 1; int d = 2; int s = 4;
+	while(s<=num){
+		r++; d+=2; s += (d+1);
+	}
+	return r;
+}
